3b21d035037295bfbcb239e7c359d71a2ad387f3
Aishwary Gupta <guptashark@gmail.com>
2018-02-10 14:02:33 -0500

    feat: Create map from fn names to fn ptrs.
    To implement a basic calculator, the program
    needs to understand basic operations like +, -
    /, *, etc. So, we map those strings to the
    functions that implement them.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,38 @@
 #include <iostream> 
 #include <string> 
+#include <map> 
 
 using namespace std;
 
+int fn_div(int a, int b) {
+	return a / b;
+}
 
+int fn_mult(int a, int b) {
+	return a * b;
+}
 
+int fn_add(int a, int b) {
+	return a + b;
+}
+
+int fn_sub(int a, int b) {
+	return a  - b;
+}
+
+	
 int main(void) {
+
+	map<string, int (*)(int, int)> functions;
+	functions["*"] = fn_mult;
+	functions["/"] = fn_div;
+	functions["+"] = fn_add;
+	functions["-"] = fn_sub;
 
 	cout << "Simple Calculator" << endl;
 	cout << "Version 1.0" << endl;
+
+	cout << functions["/"](10, 2) << endl;
 
 
 
